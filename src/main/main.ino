3681b40b206d0d98f68cb5269b597686f2d0f400
Tae Yoon Kim <the.thinkty@gmail.com>
2021-11-15 23:03:10 +0900

    Fixed handling query from BT module (HC-06)

--- src/main/main.ino
+++ src/main/main.ino
@@ -4,11 +4,8 @@ const byte BT_RX_PIN = 2;
 const byte BT_TX_PIN = 3;
 const byte LED_PIN = LED_BUILTIN; // LED for blink command
 
-const byte NONE_CMD = 0;
-const byte BLINK_CMD = 1;
-
 SoftwareSerial BTSerial(BT_RX_PIN, BT_TX_PIN);
-byte Command;
+String BTBuffer;
 
 void setup() {
   Serial.begin(9600);
@@ -17,26 +14,49 @@ void setup() {
   // Initialize HC-06 Bluetooth module
   BTSerial.begin(9600);
 
-  Command = NONE_CMD;
+  BTBuffer = "";
   pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
-  if (BTSerial.available()) {
-    // TODO: read and parse into commands
-    Serial.write(BTSerial.read());
-  }
-
   // Use AT+NAME"name" to modify the name and AT+PIN"pin" to modify the pin
   // See http://www.martyncurrey.com/arduino-and-hc-06-zs-040/ for list of commands
   if (Serial.available()) {
     BTSerial.write(Serial.read());
+  }
+
+  readFromBT();
+}
+
+// Read from HC-06 Bluetooth module
+void readFromBT() {
+  if (BTSerial.available()) {
+
+    // Start reading and store in buffer
+    while (BTSerial.available()) {
+      BTBuffer += (char)BTSerial.read();
+      delay(10); // Delay to let the arduino process the incoming bytes
+    }
+
+    // Parse and execute based on BT buffer
+    parseCommand();
+
+    // Print the buffer and clear
+    Serial.println(BTBuffer);
+    BTBuffer = "";
+  }
+}
+
+// Execute the appropriate function based on the given Command value
+void parseCommand() {
+  if (BTBuffer == "OK") {
+    blinkCmd();
   }
 }
 
 // Simple command for blinking the LED on the specified pin for half a second
 void blinkCmd() {
   digitalWrite(LED_PIN, HIGH);
-  delay(500);
+  delay(5000);
   digitalWrite(LED_PIN, LOW);
 }
