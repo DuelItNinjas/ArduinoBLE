66cbc93ab84c7338296a6fe591d7cdbee43a5dfe
Tae Yoon Kim <the.thinkty@gmail.com>
2021-11-23 22:29:58 +0900

    Added temp/hum sensor and external led

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,10 +1,13 @@
 #include <SoftwareSerial.h>
+#include <DHT.h>
 #define IR_SEND_PIN 8
 #include <IRremote.h>
 
 #define BT_RX_PIN 2
 #define BT_TX_PIN 3
-#define LED_PIN LED_BUILTIN // LED for blink command
+#define DHT_TYPE  DHT22 // AM2302
+#define DHT_PIN   4 // Temperature and Humidity
+#define LED_PIN   5 // LED for blink command
 
 // See https://gist.github.com/francis2110/8f69843dd57ae07dce80 for IR codes for LG
 #define IR_POWER    0x20DF10EF
@@ -17,7 +20,7 @@
 #define IR_RIGHT    0x20DF609F
 
 // Bluetooth characteristics for available features
-#define BTC_BLINK       '\x0'
+#define BTC_GET         '\x0'
 #define BTC_IR_POWER    '\x1'
 #define BTC_IR_SETTINGS '\x2'
 #define BTC_IR_CONFIRM  '\x3'
@@ -26,9 +29,14 @@
 #define BTC_IR_DOWN     '\x6'
 #define BTC_IR_LEFT     '\x7'
 #define BTC_IR_RIGHT    '\x8'
+#define BTC_BLINK       '\x9'
+#define BTC_TEMP        '\x10'
 
 SoftwareSerial BTSerial(2, 3);
 String BTBuffer;
+DHT dht(DHT_PIN, DHT_TYPE);
+float temp;
+float hum;
 
 void setup() {
   Serial.begin(9600);
@@ -37,8 +45,8 @@ void setup() {
   BTSerial.begin(9600);
   BTBuffer = "";
 
-  // Initialize IR transmitter
-//  IrSender.begin(IR_SEND_PIN, false, 0);
+  // Initialize Temp/Hum sensor
+  dht.begin();
 
   // Initialize LED
   pinMode(LED_PIN, OUTPUT);
@@ -76,7 +84,7 @@ void loop() {
 void parseCommand() {
   // TODO: temporary
   if (BTBuffer == "OK") {
-    irConfirm();
+    blinkCmd();
   }
 }
 
@@ -90,4 +98,20 @@ void blinkCmd() {
 // Transmit 'confirm' IR code
 void irConfirm() {
   IrSender.sendNECMSB(IR_SETTINGS, 32, false);
+}
+
+void readTemp() {
+  hum = dht.readHumidity();
+  temp = dht.readTemperature();
+
+  if (isnan(hum) || isnan(temp)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  delay(1000); // It takes a while to read from these sensors
+  Serial.print("Humidity: ");
+  Serial.print(hum);
+  Serial.print("%  Temperature: ");
+  Serial.println(temp);
 }
